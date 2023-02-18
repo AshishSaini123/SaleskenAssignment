c71461740ff8158ec0a214cb04c47076ba65d489
AshishSaini123 <aashucsengi@gmail.com>
2023-02-18 22:00:41 +0530

    completed the UI of login and signup now working on funcionality

---
+++ index.js
@@ -0,0 +1,43 @@
+
+let x=document.getElementById("login");
+let y=document.getElementById("register");
+let z=document.getElementById("btn");
+
+const Register=()=>{
+    x.style.left="-400px";
+    y.style.left="50px";
+    z.style.left="110px";
+}
+
+const Login=()=>{
+    x.style.left="50px";
+    y.style.left="450px";
+    z.style.left="0px";
+}
+
+const handleLogin=()=>{
+    event.preventDefault()
+    let email=document.querySelector(".email1").value;
+    let pass=document.querySelector(".pass1").value;
+    console.log(email,pass);
+    let users=JSON.parse(localStorage.getItem("setUsers"));
+}
+
+const handleRegister=()=>{
+    event.preventDefault()
+    let email=document.querySelector(".email2").value;
+    let pass=document.querySelector(".pass2").value;
+    console.log(email,pass);
+
+    let user={
+        email,
+        pass,
+    }
+
+    let users=JSON.parse(localStorage.getItem("setUsers")) || [];
+    users.push(user);
+    localStorage.setItem("setUsers",JSON.stringify(users));
+    let use=JSON.parse(localStorage.getItem("setUsers"));
+    console.log(use);
+
+}
